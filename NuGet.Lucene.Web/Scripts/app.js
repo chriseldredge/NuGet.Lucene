eb9210567640f9fcad04293fecbdf01d926b39f5
Chris Eldredge <chris.eldredge@gmail.com>
2013-02-24 12:25:35 -0500

    EmberJS proof of concept.

---
+++ NuGet.Lucene.Web/Scripts/app.js
@@ -0,0 +1,20 @@
+﻿App = Ember.Application.create({
+    name: "NuGet",
+    LOG_TRANSITIONS: true,
+});
+
+App.packagesController = Ember.Object.create({
+    status: undefined,
+    getStatus: function () {
+        $.getJSON('/api/status', function (data) {
+            App.packagesController.set('status', data);
+        });
+    }
+});
+
+App.StatusView = Em.View.extend({
+    templateName: 'indexing-status',
+    statusBinding: 'App.packagesController.status',
+});
+
+App.packagesController.getStatus();
