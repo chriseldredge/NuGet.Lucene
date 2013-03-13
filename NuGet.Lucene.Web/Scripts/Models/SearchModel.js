ceac4e97b5190e3ad19d556013734cdb4193508f
Chris Eldredge <chris.eldredge@gmail.com>
2013-03-13 18:24:58 -0400

    Split out PaginationSupport and SearchModel.

---
+++ NuGet.Lucene.Web/Scripts/Models/SearchModel.js
@@ -0,0 +1,61 @@
+﻿define(['ember', 'Models/PaginationSupport'], function (em, PaginationSupport) {
+    return em.Object.extend(PaginationSupport, {
+        data: {},
+        query: '',
+        loaded: false,
+        totalBinding: 'data.totalHits',
+        hits: function () {
+            var copy = [];
+            var hits = this.get('data.hits');
+            if (!hits) return copy;
+            for (var i = 0; i < hits.length; i++) {
+                var hit = hits[i];
+                var c = {};
+                for (var attr in hit) {
+                    c[attr] = hit[attr];
+                }
+                var tagQueries = [];
+                if (!hit.tags) hit.tags = '';
+                var tags = hit.tags.split(' ');
+                c.tags = [];
+                for (var j = 0; j < tags.length; j++) {
+                    if (tags[j] === '') continue;
+                    tagQueries.push(tags[j]);
+                }
+                c.tags = tagQueries;
+                copy.push(c);
+            }
+            return copy;
+        }.property('data.hits').cacheable(),
+
+        search: function (query) {
+            console.log('set query to ' + query);
+            this.setProperties({ loaded: false, page: 0, query: query });
+            this.load();
+        },
+        
+        didRequestPage: function () {
+            console.log('page changed to ' + this.get('page'));
+            this.load();
+        },
+        
+        load: function () {
+            var self = this;
+
+            console.log('load search results for query ' + this.get('query') + ' page ' + this.get('page'));
+
+            $.ajax("api/v2/package", {
+                type: 'GET',
+                data: {
+                    query: self.get('query'),
+                    offset: self.get('page') * self.get('pageSize'),
+                    count: self.get('pageSize')
+                },
+                success: function (data, status, xhr) {
+                    self.set('data', data);
+                    self.set('loaded', true);
+                }
+            });
+        },
+    });
+});
--- NuGet.Lucene.Web/Scripts/app.js
+++ NuGet.Lucene.Web/Scripts/app.js
@@ -1,127 +1,17 @@
 ﻿define([
         'ember',
-        'Models/IndexingStatus',
-/*        'app/models/user',
-        "app/views/main-views",
-        "app/views/access-views",
-        "app/views/item-views"
-*/
-    ], function(em, IndexingStatus) {
+        'Models/IndexingModel',
+        'Models/SearchModel'
+], function (em, IndexingModel, SearchModel) {
 
-        var app = em.Application.create({
-            name: "NuGet",
-        });
+    var app = em.Application.create({
+        name: "NuGet",
+    });
 
-        app.deferReadiness();
+    app.deferReadiness();
 
-        app.IndexingModel = IndexingStatus;
-
-        em.PaginationSupport = em.Mixin.create({
-            hasPaginationSupport: true,
-            total: 0,
-            page: 0,
-            pageSize: 10,
-            didRequestPage: em.K,
-
-            first: function() {
-                return this.get('page') * this.get('pageSize') + 1;
-            }.property('page', 'pageSize').cacheable(),
-
-            last: function() {
-                return Math.min((this.get('page') + 1) * this.get('pageSize'), this.get('total'));
-            }.property('page', 'pageSize', 'total').cacheable(),
-
-            hasPrevious: function() {
-                return this.get('page') > 0;
-            }.property('page').cacheable(),
-
-            hasNext: function() {
-                return this.get('last') < this.get('total');
-            }.property('last', 'total').cacheable(),
-
-            nextPage: function() {
-                if (this.get('hasNext')) {
-                    this.incrementProperty('page');
-                }
-            },
-
-            previousPage: function() {
-                if (this.get('hasPrevious')) {
-                    this.decrementProperty('page');
-                }
-            },
-
-            totalPages: function() {
-                return Math.ceil(this.get('total') / this.get('pageSize'));
-            }.property('total', 'pageSize').cacheable(),
-
-            pageDidChange: function() {
-                this.didRequestPage(this.get('page'));
-            }.observes('page')
-        });
-
-        app.Search = em.Object.extend(em.PaginationSupport, {
-            data: {},
-            query: '',
-            loaded: false,
-            totalBinding: 'data.totalHits',
-            hits: function() {
-                var copy = [];
-                var hits = this.get('data.hits');
-                if (!hits) return copy;
-                for (var i = 0; i < hits.length; i++) {
-                    var hit = hits[i];
-                    var c = {};
-                    for (var attr in hit) {
-                        c[attr] = hit[attr];
-                    }
-                    var tagQueries = [];
-                    if (!hit.tags) hit.tags = '';
-                    var tags = hit.tags.split(' ');
-                    c.tags = [];
-                    for (var j = 0; j < tags.length; j++) {
-                        if (tags[j] === '') continue;
-                        tagQueries.push(tags[j]);
-                    }
-                    c.tags = tagQueries;
-                    copy.push(c);
-                }
-                return copy;
-            }.property('data.hits').cacheable(),
-
-            search: function(query) {
-                console.log('set query to ' + query);
-                this.set('loaded', false);
-                this.setProperties({ page: 0, query: query });
-            },
-
-            queryAndPage: function() {
-                return { query: this.get('query'), page: this.get('page') };
-            }.property('query', 'page').cacheable(),
-
-            queryOrPageChanged: function() {
-                this.load();
-            }.observes('queryAndPage'),
-
-            load: function() {
-                var self = this;
-
-                console.log('load search results for query ' + this.get('query') + ' page ' + this.get('page'));
-
-                $.ajax("api/v2/package", {
-                    type: 'GET',
-                    data: {
-                        query: self.get('query'),
-                        offset: self.get('page') * self.get('pageSize'),
-                        count: self.get('pageSize')
-                    },
-                    success: function(data, status, xhr) {
-                        self.set('data', data);
-                        self.set('loaded', true);
-                    }
-                });
-            },
-        });
+    app.IndexingModel = IndexingModel;
+    app.Search = SearchModel;
 
         app.ApplicationController = em.Controller.extend({
             needs: 'search',
@@ -133,11 +23,9 @@
 
         app.SearchController = em.ObjectController.extend({
             query: '',
-            search: function(query) {
-                console.log('searchController.search ' + query);
-                console.log('searchController model ' + this.get('model'));
+            search: function (query) {
                 this.set('query', query);
-                this.get('model').search(query);
+                this.get('model').search(this.get('query'));
             },
             nextPage: function() {
                 this.get('model').nextPage();
@@ -176,14 +64,14 @@
         });
 
         app.SearchRoute = em.Route.extend({
-            setupController: function(controller, params) {
-                var query = params;
-                if (params && params.query) {
-                    query = params.query;
-                }
-
+            setupController: function(controller, context) {
                 controller.set('model', app.Search.create());
-                controller.search(query);
+                
+                if (typeof context === "string") {
+                    controller.search(context);
+                } else {
+                    controller.search(context.query);
+                }
             }
         });
 
