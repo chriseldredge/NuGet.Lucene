8db89204128e577f79bdfb8452f8dbdee020382a
Chris Eldredge <chris.eldredge@gmail.com>
2014-09-13 20:52:32 +0100

    Add constructors to WebHost settings.

--- source/NuGet.Lucene.Web/NuGetWebApiWebHostSettings.cs
+++ source/NuGet.Lucene.Web/NuGetWebApiWebHostSettings.cs
@@ -1,10 +1,19 @@
-﻿using System.IO;
+﻿using System.Collections.Specialized;
+using System.IO;
 using System.Web.Hosting;
 
 namespace NuGet.Lucene.Web
 {
     public class NuGetWebApiWebHostSettings : NuGetWebApiSettings
     {
+        public NuGetWebApiWebHostSettings(string prefix) : base(prefix)
+        {
+        }
+
+        public NuGetWebApiWebHostSettings(string prefix, NameValueCollection settings, NameValueCollection roleMappings) : base(prefix, settings, roleMappings)
+        {
+        }
+
         protected override string MapPathFromAppSetting(string key, string defaultValue)
         {
             var path = base.GetAppSetting(key, defaultValue);
