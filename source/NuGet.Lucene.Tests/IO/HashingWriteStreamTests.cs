90999283fe7703b8ec4066640c3b6ca887bed538
Chris Eldredge <chris.eldredge@gmail.com>
2014-11-05 16:05:44 +0000

    Add HashingWriteStream that wraps a Stream and computes hash as stream is written.

---
+++ source/NuGet.Lucene.Tests/IO/HashingWriteStreamTests.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using NuGet.Lucene.IO;
+using NUnit.Framework;
+
+namespace NuGet.Lucene.Tests.IO
+{
+    [TestFixture]
+    class HashingWriteStreamTests
+    {
+        private static readonly string text = new string('c', 8192);
+        private static readonly byte[] textBytes = Encoding.UTF8.GetBytes(text);
+
+        [Test]
+        public void Write()
+        {
+            var source = new MemoryStream();
+            var stream = new HashingWriteStream(null, source, new SHA256CryptoServiceProvider());
+
+            stream.Write(textBytes, 0, textBytes.Length);
+            
+            Assert.That(Encoding.UTF8.GetString(source.ToArray()), Is.EqualTo(text));
+        }
+
+        [Test]
+        public void ComputesHash()
+        {
+            var stream = new HashingWriteStream(null, new MemoryStream(), new SHA256CryptoServiceProvider());
+
+            stream.Write(textBytes, 0, textBytes.Length);
+            stream.Close();
+
+            var expectedHash = new SHA256CryptoServiceProvider().ComputeHash(textBytes);
+
+            Assert.That(stream.Hash, Is.EqualTo(expectedHash));
+        }
+
+        [Test]
+        public async Task CopyToAsync_Writes()
+        {
+            var source = new MemoryStream(textBytes);
+            var dest = new MemoryStream();
+            var stream = new HashingWriteStream(null, dest, new SHA256CryptoServiceProvider());
+
+            await source.CopyToAsync(stream);
+
+            Assert.That(dest.ToArray(), Is.EqualTo(textBytes));
+        }
+
+        [Test]
+        public async Task CopyToAsync_ComputesHash()
+        {
+            var source = new MemoryStream(textBytes);
+            var dest = new MemoryStream();
+            var stream = new HashingWriteStream(null, dest, new SHA256CryptoServiceProvider());
+
+            await source.CopyToAsync(stream);
+            stream.Close();
+
+            var expectedHash = new SHA256CryptoServiceProvider().ComputeHash(textBytes);
+            Assert.That(stream.Hash, Is.EqualTo(expectedHash));
+        }
+    }
+}
---
+++ source/NuGet.Lucene/IO/HashingWriteStream.cs
@@ -0,0 +1,128 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NuGet.Lucene.IO
+{
+    public class HashingWriteStream : Stream
+    {
+        private readonly string fileLocation;
+        private readonly Stream stream;
+        private readonly HashAlgorithm hashAlgorithm;
+        private bool disposed;
+        private byte[] hash;
+
+        public HashingWriteStream(string fileLocation, Stream stream, HashAlgorithm hashAlgorithm)
+        {
+            this.fileLocation = fileLocation;
+            this.stream = stream;
+            this.hashAlgorithm = hashAlgorithm;
+        }
+
+        public byte[] Hash
+        {
+            get
+            {
+                if (!disposed)
+                {
+                    throw new InvalidOperationException("Stream must be closed before Hash can be computed.");
+                }
+                return hash;
+            }
+        }
+
+        public string FileLocation
+        {
+            get { return fileLocation; }
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            stream.Write(buffer, offset, count);
+            hashAlgorithm.TransformBlock(buffer, offset, count, null, 0);
+        }
+
+        public override void Flush()
+        {
+            stream.Flush();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (!disposing || disposed) return;
+
+            stream.Dispose();
+            hashAlgorithm.TransformFinalBlock(new byte[0], 0, 0);
+            hash = hashAlgorithm.Hash;
+            hashAlgorithm.Dispose();
+            disposed = true;
+        }
+
+#if NET_4_5
+        public override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            await stream.WriteAsync(buffer, offset, count, cancellationToken);
+            hashAlgorithm.TransformBlock(buffer, offset, count, null, 0);
+        }
+
+        public override Task FlushAsync(CancellationToken cancellationToken)
+        {
+            return stream.FlushAsync(cancellationToken);
+        }
+#endif
+
+        public override IAsyncResult BeginWrite(byte[] buffer, int offset, int count, AsyncCallback callback, object state)
+        {
+            throw new NotSupportedException("Use WriteAsync.");
+        }
+
+        public override void WriteByte(byte value)
+        {
+            throw new NotSupportedException();
+        }
+
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override bool CanRead
+        {
+            get { return false; }
+        }
+
+        public override bool CanSeek
+        {
+            get { return false; }
+        }
+
+        public override bool CanWrite
+        {
+            get { return true; }
+        }
+
+        public override long Length
+        {
+            get { return stream.Length; }
+        }
+
+        public override long Position
+        {
+            get { return stream.Position; }
+            set { throw new NotSupportedException(); }
+        }
+    }
+}
